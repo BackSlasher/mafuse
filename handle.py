23e146541a1fd53a121451fbad8fe8c0e2a7884a
Nitzan Raz <nitzan@photomania.net>
2014-05-28 08:22:49 +0300

    mount works now

--- handle.py
+++ handle.py
@@ -2,10 +2,11 @@
 # mount / unmount script for mafuse - managed afuse system
 # usage: handle.py <afusetab> <afusemount> <directory> <action>
 # afusetab  : the file containing the afuse configuration
-# afusemount: the directory where afuse was mounted (NOT the directory to be unmounted) (%r)
+# afusemount: the directory where afuse was mounted (NOT the directory to be unmounted)
+# subdirectory: the short name of the directory to mount / unmount (%r)
 # directory : the fully qualified directory to mount / unmount (%m)
 # action    : whether to mount or unmount. can be 'mount' or 'unmount'
-# usage from afuse (as intended): ./handle.py <file> %r %m <mount/unmount>
+# usage from afuse (as intended): ./handle.py <file> <afusemount> %r %m <mount/unmount>
 
 from __future__ import print_function
 import sys
@@ -13,25 +14,32 @@ import os
 import yaml
 import re
 
-if len(sys.argv)!=5:
+if len(sys.argv)!=6:
     print("Wrong argument count. usage: mount.py <afusetab> <afusemount> <directory> <action>", file=sys.stderr)
     sys.exit(1)
 elif not os.path.exists(sys.argv[1]):
     print("afusetab not found", file=sys.stderr)
     sys.exit(1)
-# Now we can assume things are ok
-with open(sys.argv[1],'r') as f:
-    doc = yaml.load(f)
 
-if doc is None:
+try:
+    with open(sys.argv[1],'r') as f:
+        doc = yaml.load(f)
+except:
     print("afusetab couldn't load", file=sys.stderr)
     sys.exit(2)
 
 # find the right root, then the right directory
 afusemount=sys.argv[2]
-directory=sys.argv[3]
-subdirectory=re.sub('^%s/' % re.escape(sys.argv[2]),'',sys.argv[3])
-directive=sys.argv[4]
+subdirectory=sys.argv[3]
+directory=sys.argv[4]
+directive=sys.argv[5]
+
+import pprint
+pprint.pprint(sys.argv)
+
+# ignore .Trash
+if re.search("^\.Trash",subdirectory): exit(0)
+
 try:
     command = doc[afusemount][subdirectory][directive]
 except:
@@ -40,5 +48,8 @@ except:
 
 command=command.replace('%r',afusemount)
 command=command.replace('%m',directory)
-print(command)
-#os.system(command)
+#print(command)
+
+res = os.system(command)
+#from subprocess import call
+#call(['notify-send',command])
---
+++ init.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# Initializes afuse according to file
+# usage: ./init.py [afusetab]
+# afusetab: list of afuse mounts to init. If none are present, local file named afusetab is assumed.
+
+from __future__ import print_function
+import sys
+import os
+import yaml
+import re
+
+if len(sys.argv)>2:
+    print("Wrong argument count. usage: handle.py [afusetab]", file=sys.stderr)
+    sys.exit(1)
+afusetab='afusetab'
+if len(sys.argv)==2: afusetab=sys.argv[1]
+if not os.path.exists(afusetab):
+    print("afusetab not found", file=sys.stderr)
+    sys.exit(1)
+
+try:
+    with open(afusetab,'r') as f:
+        doc = yaml.load(f)
+except:
+    print("afusetab couldn't load", file=sys.stderr)
+    sys.exit(2)
+
+# script paths:
+d = os.path.dirname(os.path.realpath(__file__))
+handles="%s/handle.py" % (d)
+lists="%s/list.py" % (d)
+
+# find all roots in the document, and for every one of those invoke a proper command
+for root in doc:
+    mounttemplate='mount_template=%s %s %s %%r %%m mount' % (handles,afusetab,root)
+    unmounttemplate='unmount_template=%s %s %s %%r %%m unmount' % (handles,afusetab,root)
+    poproottemplate='"populate_root_command=%s %s %s"' % (lists,afusetab,root)
+    callarr = ['afuse','-o',mounttemplate,'-o',unmounttemplate,root,'-f']
+    #callarr = ['afuse','-o','mount_template=sshfs boredstorage@oneboredadmin.com:. %m','-o','unmount_template=fusermount -u -z %m',root]
+    #callarr = ['afuse','-o',mounttemplate,'-o',unmounttemplate,'-o',poproottemplate,root]
+    import pprint
+    #pprint.pprint(callarr)
+    from subprocess import call
+    call(callarr)
