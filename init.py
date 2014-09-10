1aca68940d2b7dfed7f37d16ce10b0a3db8fb1df
Nitzan Raz <nitzan@photomania.net>
2014-09-10 13:51:43 +0300

    All working

--- init.py
+++ init.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
+
 # Initializes afuse according to file
 # usage: ./init.py [afusetab]
 # afusetab: list of afuse mounts to init. If none are present, local file named afusetab is assumed.
@@ -11,24 +12,10 @@ import re
 import argparse
 
 parser = argparse.ArgumentParser(description='Initialize afuse according to a file')
-parser.add_argument('afusetab',nargs='?',default=None,help='Location of the afusetab file to parse')
+parser.add_argument('afusetab',nargs='?',default=None,help='Location of the afusetab file to parse. Defaults to "afusetab" located near script')
 parser.add_argument('-f','--foreground',help='run afuse in foreground (for troubleshooting)',action='store_true')
 args = parser.parse_args()
-'''
-if len(sys.argv)>2:
-    print("Wrong argument count. usage: handle.py [afusetab]", file=sys.stderr)
-    sys.exit(1)
-afusetab='afusetab'
-if len(sys.argv)==2: afusetab=sys.argv[1]
-if not os.path.exists(afusetab):
-    print("afusetab not found", file=sys.stderr)
-    sys.exit(1)
-'''
-'''
-try:
-    with open(afusetab,'r') as f:
-        doc = yaml.load(f)
-'''
+
 afusetab = args.afusetab
 if afusetab is None:
     afusetab = os.path.join(os.path.dirname(__file__),'afusetab')
@@ -36,8 +23,9 @@ try:
     with open(afusetab,'r') as f:
         doc = yaml.load(f)
 except:
-    print("couldn't prase afusetab", file=sys.stderr)
-    sys.exit(2)
+    raise Exception("couldn't prase afusetab. Make sure it exists and valid")
+    #print("couldn't prase afusetab. Make sure it exists and valid", file=sys.stderr)
+    #sys.exit(2)
 
 # script paths:
 d = os.path.dirname(os.path.abspath(__file__))
@@ -54,7 +42,7 @@ for root in doc:
     mounttemplate='mount_template=%s %s %s %%r %%m mount' % (handles,afusetab,root)
     unmounttemplate='unmount_template=%s %s %s %%r %%m unmount' % (handles,afusetab,root)
     poproottemplate='populate_root_command=%s %s \'%s\'' % (lists,afusetab,root)
-    callarr = ['afuse','-o',mounttemplate,'-o',unmounttemplate,'-o',poproottemplate,realroot]
+    callarr = ['afuse','-o','nonempty','-o',mounttemplate,'-o',unmounttemplate,'-o',poproottemplate,realroot]
     if(args.foreground): callarr.append('-f')
     from subprocess import call
     call(callarr)
